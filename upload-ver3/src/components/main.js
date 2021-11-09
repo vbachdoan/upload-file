0115169ffc527d560a273dac5031336a61ab5c28
vbachdoan <vbachdoan@gmail.com>
2021-11-10 02:40:01 +0700

    note for back-end:
    -Check can I post to http://localhost:8080/upload
    -Check can I get list files were stored in database at http://localhost:8080/files
    -Get delete request at http://localhost:8080/delete, delete file in database
    ***************************
    Type of request:
    	/upload: array of objects
    	/files: what is your response type ?
    	/delete: single object

--- upload-ver3/src/components/main.js
+++ upload-ver3/src/components/main.js
@@ -5,53 +5,81 @@ export default class Main extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            listFiles : []
+            listFiles : undefined,
+            style: {
+                width: '0',
+            }
         };
         this.handleChange=this.handleChange.bind(this);
         this.handleUpload=this.handleUpload.bind(this);
+        this.handleDelete=this.handleDelete.bind(this);
     }
 
     handleChange(event){
         let selected = event.target.files;
+        let passData = [];
+        for(let i=0;i<selected.length;i++){
+            passData.push(selected[i])
+        }
         this.setState({
-            listFiles : selected,
+            listFiles : passData,
         })
     }
 
     handleUpload(){
 
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://localhost:8080/admin/upload");
+        xhr.open("POST", "http://localhost:8080/upload");
 
-        // xhr.upload.addEventListener('progress',(event)=>{
-        //     let fileLoaded = Math.floor((event.loaded / event.total) * 100);
-        //     console.log(fileLoaded)
-        //     this.setState({
-        //         style:{
-        //             width: `${fileLoaded}%`
-        //         }
-        //     })
-        // })
+        xhr.upload.addEventListener('progress',(event)=>{
+            let fileLoaded = Math.floor((event.loaded / event.total) * 100);
+            console.log(fileLoaded)
+            this.setState({
+                style:{
+                    width: `${fileLoaded}%`
+                }
+            })
+        })
 
         let formData = new FormData();
         for(let i=0;i<this.state.listFiles.length;i++){
-            formData.append('file',this.state.listFiles[i]);
+            formData.append('files',this.state.listFiles[i]);
         }  
 
-        xhr.send(formData)
-        console.log(formData);
+        xhr.send(formData);
+
+    }
+
+    handleDelete(item){
+        const listFiles = this.state.listFiles.filter(i => i!== item);
+        this.setState({listFiles});
     }
 
     render(){
         return(
             <>
             <div id="wrap-upload-field">
-                <form action="#" onClick={this.handleClick} onChange={this.handleChange} id="upload-box">
+                <form action="#" onChange={this.handleChange} id="upload-box">
                     <input className="file-input" type="file" id="upload-field" hidden multiple/>
                     <label htmlFor="upload-field" id="upload-field-label">Upload Files</label>
                 </form>
+                <div id="display-selected-files">
+                    {
+                        this.state.listFiles && 
+                        <div id="wrap-selected">
+                            {this.state.listFiles.map((file)=>
+                                <div className="file-block">
+                                    <p>File type: {file.type}</p>
+                                    <p>File name: {file.name}</p>
+                                    <div className="delete-btn" onClick={this.handleDelete.bind(this, file)}>Delete</div>
+                                </div>
+                            )}
+                        </div>
+                    }
+                    <div id="btn" onClick={this.handleUpload}>Upload</div>
+                    <div id="progress"><span style={this.state.style}></span></div>
+                </div>
             </div>
-            <div id="btn" onClick={this.handleUpload}>Upload</div>
             </>
 
         )
